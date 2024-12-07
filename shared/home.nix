58b9a20f1821b8137f13949ebb685585735f7ba8
Simon Riezebos <info@datagiant.org>
2024-12-07 17:08:40 +0100

    Enable flakes everywhere

--- shared/home.nix
+++ shared/home.nix
@@ -348,6 +348,11 @@
   fonts.fontconfig.enable = true;
   programs.home-manager.enable = true;
 
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = ["nix-command" "flakes"];
+  };
+
   # below is needed for Spotlight but Raycast is smart enough to read symlinks
   # home.activation = {
   #   aliasHomeManagerApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ""
