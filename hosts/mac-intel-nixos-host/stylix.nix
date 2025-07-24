08d6964e09a63ffd36111f48f6f8cbf35441ec46
Job Visser <job@dutchdataworks.nl>
2025-07-24 12:57:36 +0200

    change font

--- hosts/mac-intel-nixos-host/stylix.nix
+++ hosts/mac-intel-nixos-host/stylix.nix
@@ -14,10 +14,16 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     image = ../../non-nix-configs/nix-wallpaper-binary-black.png;
     polarity = "dark";
+    fonts = {
+      sizes = {
+        desktop = 12;
+        applications = 12;
+      };
+    };
     cursor = {
       package = pkgs.capitaine-cursors-themed;
       name = "Capitaine Cursors (Gruvbox)";
-      size = 30;
+      size = 28;
     };
   };
 }
