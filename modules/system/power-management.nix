ff823f43fdd7f7507dfdc82cb714457982d56bc8
Job Visser <job@dutchdataworks.nl>
2025-12-01 18:34:32 +0100

    done p management

--- modules/system/power-management.nix
+++ modules/system/power-management.nix
@@ -7,6 +7,9 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
