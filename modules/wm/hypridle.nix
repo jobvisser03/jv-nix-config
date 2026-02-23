b7d6135ea58951e189f22ffe38795b5ddf070a7e
Job Visser <job@dutchdataworks.nl>
2026-02-23 09:06:13 +0100

    don't logout on lock

--- modules/wm/hypridle.nix
+++ modules/wm/hypridle.nix
@@ -5,29 +5,16 @@
       # Lock when suspend
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
+        before_sleep_cmd = "pidof hyprlock || hyprlock";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listener = [
         # Lock screen after n seconds
         {
-          timeout = 300;
-          on-timeout = "loginctl lock-session";
+          timeout = 1200;
+          on-timeout = "pidof hyprlock || hyprlock";
         }
-        # After display off or suspend it takes VERY
-        # long to wake up and the kboard and mouse do not work
-        # # Turn monitor off n seconds
-        # {
-        #   timeout = 350;
-        #   on-timeout = "hyprctl dispatch dpms off";
-        #   on-resume = "hyprctl dispatch dpms on";
-        # }
-        # #  complete suspend after n seconds
-        # {
-        #   timeout = 999999999;
-        #   on-timeout = "systemctl suspend";
-        # }
       ];
     };
   };
