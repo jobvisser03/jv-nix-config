57e07faf85380b66cd60e4fbaa0417d6679d9674
Job Visser <job.visser@enexis.nl>
2026-03-27 21:19:39 +0100

    kitty with trailing cursor coolness

--- modules/desktop/terminals/kitty.nix
+++ modules/desktop/terminals/kitty.nix
@@ -7,6 +7,17 @@
     config,
     ...
   }: {
-    programs.kitty.enable = true;
+    programs.kitty = {
+      enable = true;
+      settings = {
+        cursor_trail = 3;
+        cursor_trail_decay = "0.1 0.4";
+        window_padding_width = 20;
+      };
+
+      keybindings = {
+        "ctrl+shift+t" = "new_tab_with_cwd";
+      };
+    };
   };
 }
