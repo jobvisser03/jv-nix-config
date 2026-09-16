1fb18d0b0df4d4e0f61393d23be6cd2b89e66480
Job Visser <job@dutchdataworks.com>
2026-09-16 12:15:56 +0200

    pin darwin

--- flake.nix
+++ flake.nix
@@ -42,10 +42,10 @@
     };
 
     herdr.url = "github:ogulcancelik/herdr/v0.9.0";
-    
+
     darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs-devenv";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix4vscode = {
